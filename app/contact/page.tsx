56c795262260c3078627f77a0c69b00a107029d9
Saurav <saurav@ignite.com>
2026-03-16 15:19:01 +0530

    minor changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -103,7 +103,7 @@ export default function ContactPage() {
                 <svg className="contact-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <a href="tel:1423141" className="person-phone">1423141</a>
+                <a href="tel:1423141" className="person-phone">7218753440</a>
               </div>
             </div>
           </div>
@@ -124,7 +124,7 @@ export default function ContactPage() {
             </div>
             <p className="location-text">
               Maharashtra Institute of Technology<br />
-              Chhatrapati Sambhajinagar, Maharashtra
+              Chhatrapati Sambhajinagar, Maharashtra -431010
             </p>
           </div>
         </div>
